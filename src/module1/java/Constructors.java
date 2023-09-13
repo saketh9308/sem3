887a1f6de377a0a27fbf1b0ce99b1fc98d61906f
SakethPathike <sakethpathike@gmail.com>
2023-09-13 21:40:25 +0530

    🗿

---
+++ src/module1/java/Constructors.java
@@ -0,0 +1,24 @@
+package module1.java;
+
+public class Constructors {
+    int _no;
+    String _name;
+
+    public Constructors(int no, String name) {
+        _no = no;
+        _name = name;
+    }
+
+    public static void main(String args[]) {
+        Constructors constructors = new Constructors(0, "sample text");
+        System.out.println(constructors._name + "\n" + constructors._no);
+    }
+}
+
+/*
+
+Output:
+sample text
+0
+
+ */
